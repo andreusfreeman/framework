62768f1dbcb16cc1944e3799719f7ad1f55216ea
andreusfreeman <andriy.natsyk@gmail.com>
2016-04-17 22:55:12 +0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,2 +1,2 @@
-window.onload = function() {window.frames["loading-site-frame"].contentWindow.document.documentElement.childNodes[2].onclick = function(e){alert(e.target);};
+function gotTest() {window.frames["loading-site-frame"].contentWindow.document.documentElement.childNodes[2].onclick = function(e){alert(e.target);};
 }
